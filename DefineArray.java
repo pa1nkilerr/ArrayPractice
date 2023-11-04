5b083c2d29a6c890b7c2fb69fb5a115b2a51fb54
pa1nkilerr <129454773+pa1nkilerr@users.noreply.github.com>
2023-11-03 20:59:32 -0500

    Add files via upload

---
+++ DefineArray.java
@@ -0,0 +1,23 @@
+package inputCalculations;
+
+import java.util.Arrays;
+
+public class DefineArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[]animal= new String[6];
+		
+		animal[0]= "Cat";
+		animal[1]= "Dog";
+		animal[2]= "Bird";
+		animal[3]= "Hamster";
+		animal[4]= "Rabbit";
+		animal[5]= "Lion";
+		
+		System.out.println(animal[5]);
+
+	}
+
+}
